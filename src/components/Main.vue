db11846aea4800c92288f067c8818439a114917f
gwbbest@vip.qq.com <gwbbest@vip.qq.com>
2018-08-27 22:42:23 +0800

    初始化...

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.main {
+  border: 1px red solid;
+  width: 100%;
+}
+</style>
